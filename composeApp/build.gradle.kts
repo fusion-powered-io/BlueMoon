728f16c6df7b01c6e0c516f470ad4494e9faea2f
Jessewb786 <jesse.brandao@fusionpowered.io>
2026-01-30 11:54:06 +0100

    fix: Improved icon appearance on android 11

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
 
     compilerOptions {
         optIn.addAll(
+            "kotlin.ExperimentalUnsignedTypes",
             "com.google.accompanist.permissions.ExperimentalPermissionsApi"
         )
         freeCompilerArgs.addAll(
