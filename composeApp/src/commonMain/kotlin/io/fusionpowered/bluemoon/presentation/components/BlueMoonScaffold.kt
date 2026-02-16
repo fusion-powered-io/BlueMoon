46717ae4673e6f9efbdf3050cce8784d8edbbb2a
Jessewb786 <jesse.brandao@fusionpowered.io>
2026-02-16 12:13:33 +0100

    feature: Added Blackout Mode for OLED screens

--- composeApp/src/commonMain/kotlin/io/fusionpowered/bluemoon/presentation/components/BlueMoonScaffold.kt
+++ composeApp/src/commonMain/kotlin/io/fusionpowered/bluemoon/presentation/components/BlueMoonScaffold.kt
@@ -1,14 +1,20 @@
 package io.fusionpowered.bluemoon.presentation.components
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.BottomSheetScaffoldState
@@ -16,6 +22,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
@@ -25,19 +32,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import compose.icons.TablerIcons
+import compose.icons.tablericons.Bulb
+import compose.icons.tablericons.CirclePlus
 import compose.icons.tablericons.Keyboard
 import compose.icons.tablericons.MinusVertical
 import compose.icons.tablericons.Mouse
 import io.fusionpowered.bluemoon.domain.bluetooth.BluetoothClient
+import io.fusionpowered.bluemoon.domain.bluetooth.BluetoothSettings
 import io.fusionpowered.bluemoon.domain.bluetooth.model.ConnectionState
 import io.fusionpowered.bluemoon.presentation.components.BlueMoonScaffold.State.SheetContent
 import kotlinx.coroutines.CoroutineScope
@@ -49,6 +63,7 @@ object BlueMoonScaffold {
     @Composable
     fun present(
         bluetoothClient: BluetoothClient = koinInject(),
+        bluetoothSettings: BluetoothSettings = koinInject(),
     ): State {
         val connected by bluetoothClient.connectionStateFlow.collectAsStateWithLifecycle(ConnectionState.Disconnected)
         val sheetContent = remember { mutableStateOf(SheetContent.Touchpad) }
@@ -57,6 +72,8 @@ object BlueMoonScaffold {
                 skipHiddenState = false
             )
         )
+        var blackOutState by remember { mutableStateOf(false) }
+
 
         LaunchedEffect(connected) {
             when (connected) {
@@ -71,16 +88,32 @@ object BlueMoonScaffold {
             }
         }
 
-        return State(
-            sheetContent = sheetContent,
-            scaffoldState = scaffoldState
-        )
+        return when {
+            blackOutState -> State.BlackedOut(
+                onBlackoutClick = { blackOutState = false }
+            )
+
+            else -> State.Normal(
+                onPairClick = { bluetoothSettings.launch() },
+                onBlackoutClick = { blackOutState = true },
+                sheetContent = sheetContent,
+                scaffoldState = scaffoldState
+            )
+        }
     }
 
-    data class State(
-        val sheetContent: MutableState<SheetContent>,
-        val scaffoldState: BottomSheetScaffoldState,
-    ) {
+    sealed interface State {
+
+        data class BlackedOut(
+            val onBlackoutClick: () -> Unit = {},
+        ) : State
+
+        data class Normal(
+            val onPairClick: () -> Unit = {},
+            val onBlackoutClick: () -> Unit = {},
+            val sheetContent: MutableState<SheetContent>,
+            val scaffoldState: BottomSheetScaffoldState,
+        ) : State
 
         enum class SheetContent {
             Touchpad,
@@ -96,33 +129,142 @@ object BlueMoonScaffold {
         keyboardPresenter: @Composable () -> BluetoothKeyboard.State = { BluetoothKeyboard.present() },
         content: @Composable () -> Unit,
     ) {
-        val state = presenter()
-        BottomSheetScaffold(
-            modifier = modifier,
-            scaffoldState = state.scaffoldState,
-            sheetShape = RectangleShape,
-            sheetDragHandle = {
-                SheetDragHandle(
-                    sheetContent = state.sheetContent,
-                    sheetState = state.scaffoldState.bottomSheetState
-                )
-            },
-            sheetContent = {
-                SheetContent(
-                    sheetContent = state.sheetContent,
-                    touchpadPresenter = touchpadPresenter,
-                    keyboardPresenter = keyboardPresenter
-                )
-            },
-            // THEME OVERRIDES:
-            sheetContainerColor = Color.Transparent, // Makes the M3 surface invisible
-            sheetSwipeEnabled = true,
-            // Optional: ensure the main content background is the PS2 Dark Blue/Black
-            containerColor = Color(0xFF00050A)
-        ) {
-            content()
+        when (val state = presenter()) {
+            is State.BlackedOut -> {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    onClick = state.onBlackoutClick,
+                    color = Color.Black,
+                ) {
+
+                }
+            }
+
+            is State.Normal -> {
+                BottomSheetScaffold(
+                    modifier = modifier,
+                    scaffoldState = state.scaffoldState,
+                    sheetShape = RectangleShape,
+                    topBar = {
+                        TopBar(
+                            onPairClick = state.onPairClick,
+                            onBlackoutClick = state.onBlackoutClick,
+                        )
+                    },
+                    sheetDragHandle = {
+                        SheetDragHandle(
+                            sheetContent = state.sheetContent,
+                            sheetState = state.scaffoldState.bottomSheetState
+                        )
+                    },
+                    sheetContent = {
+                        SheetContent(
+                            sheetContent = state.sheetContent,
+                            touchpadPresenter = touchpadPresenter,
+                            keyboardPresenter = keyboardPresenter,
+                        )
+                    },
+                    // THEME OVERRIDES:
+                    sheetContainerColor = Color.Transparent, // Makes the M3 surface invisible
+                    sheetSwipeEnabled = true,
+                    containerColor = Color.Transparent
+                ) {
+                    content()
+                }
+            }
         }
     }
+
+    @Composable
+    private fun TopBar(
+        modifier: Modifier = Modifier,
+        onPairClick: () -> Unit = {},
+        onBlackoutClick: () -> Unit = {},
+    ) {
+        Row(
+            modifier = modifier
+                .background(
+                    Brush.verticalGradient(
+                        0.0f to Color(0xFF6C7E9E),
+                        1.0f to Color(0xFF9CB2D7),
+                    )
+                )
+                .border(0.5.dp, Color.White.copy(alpha = 0.12f), RoundedCornerShape(2.dp))
+                .fillMaxWidth()
+                .height(52.dp)
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            PairNewDeviceButton(onClick = onPairClick)
+            BlackoutButton(onClick = onBlackoutClick)
+        }
+    }
+
+    @Composable
+    fun PairNewDeviceButton(
+        modifier: Modifier = Modifier,
+        onClick: () -> Unit,
+    ) {
+        Surface(
+            modifier = modifier.wrapContentSize(),
+            color = Color.Transparent,
+            onClick = onClick,
+        ) {
+            Row(
+                modifier = Modifier.wrapContentSize()
+                    .background(Color.Transparent),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Surface(
+                    shape = CircleShape,
+                    color = Color.Transparent,
+                    border = BorderStroke(2.dp, Color.DarkGray),
+                    modifier = Modifier.size(28.dp)
+                ) {
+                    Icon(
+                        imageVector = TablerIcons.CirclePlus,
+                        contentDescription = null,
+                        modifier = Modifier.padding(2.dp),
+                        tint = Color.DarkGray
+                    )
+                }
+
+                Spacer(Modifier.width(12.dp))
+
+                Text(
+                    text = "Pair",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = 0.5.sp,
+                        color = Color.DarkGray
+                    )
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun BlackoutButton(
+        modifier: Modifier = Modifier,
+        onClick: () -> Unit,
+    ) {
+        Surface(
+            modifier = modifier.wrapContentSize(),
+            onClick = onClick,
+            color = Color.Transparent,
+        ) {
+            Icon(
+                modifier = Modifier.size(36.dp),
+                imageVector = TablerIcons.Bulb,
+                contentDescription = null,
+                tint = Color.DarkGray
+            )
+        }
+    }
+
 
     @Composable
     private fun SheetDragHandle(
