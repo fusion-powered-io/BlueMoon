b52434903a8b4541317bc48d85c4b5fd85ab4008
Jessewb786 <jesse.brandao@fusionpowered.io>
2026-02-18 16:59:39 +0100

    chore: Remove some unnecessary comments and annotations

--- composeApp/src/jvmMain/kotlin/io/fusionpowered/bluemoon/MainApplication.kt
+++ composeApp/src/jvmMain/kotlin/io/fusionpowered/bluemoon/MainApplication.kt
@@ -1,7 +1,7 @@
 package io.fusionpowered.bluemoon
 
 import androidx.compose.ui.window.application
-import io.fusionpowered.bluemoon.presentation.mainActivity
+import io.fusionpowered.bluemoon.presentation.MainActivity
 import org.koin.core.annotation.KoinApplication
 import org.koin.ksp.generated.startKoin
 
@@ -15,7 +15,7 @@ fun main() {
     }
 
     application {
-        mainActivity()
+        MainActivity()
     }
 }
 
--- composeApp/src/jvmMain/kotlin/io/fusionpowered/bluemoon/presentation/MainActivity.kt
+++ composeApp/src/jvmMain/kotlin/io/fusionpowered/bluemoon/presentation/MainActivity.kt
@@ -3,13 +3,11 @@ package io.fusionpowered.bluemoon.presentation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Window
-import org.koin.core.annotation.KoinExperimentalAPI
 
 
-@OptIn(KoinExperimentalAPI::class)
 @Composable
 context(applicationScope: ApplicationScope)
-fun mainActivity() {
+fun MainActivity() {
     Window(
         onCloseRequest = applicationScope::exitApplication,
         title = "BlueMoon",
